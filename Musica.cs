37ae254aad5640c47ab71165e53c4a578c00e64c
Lucas <rohrlucas.meireles@gmail.com>
2025-08-07 22:42:29 -0300

    em progresso

---
+++ Musica.cs
@@ -0,0 +1,21 @@
+class Musica
+{
+    public string nome;
+    public string artista;
+    public int duracao;
+    private bool disponivel;
+    public void ExibirFichaTecnica()
+    {
+        Console.WriteLine($"Nome: {nome}");
+        System.Console.WriteLine($"Artista: {artista}");
+        System.Console.WriteLine($"Duração: {duracao}");
+        if (disponivel)
+        {
+            Console.WriteLine("Disponível no plano.");
+        }
+        else
+        {
+            System.Console.WriteLine("Adquira o plano Plus+");
+        }
+    }
+}
