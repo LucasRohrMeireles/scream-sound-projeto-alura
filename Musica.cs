3e9662f1d9c511108193222befc7137ff81cbdce
Lucas <rohrlucas.meireles@gmail.com>
2025-08-24 21:32:31 -0300

    aula 4

--- Musica.cs
+++ Musica.cs
@@ -1,7 +1,12 @@
 class Musica
 {
-    public string Nome { get; set; }
-    public string Artista { get; set; }
+    public Musica(Banda artista, string nome)
+    {
+        Artista = artista;
+        Nome = nome;
+    }
+    public string Nome { get; }
+    public Banda Artista { get; }
     public int Duracao { get; set; }
     public bool Disponivel { get; set; }
     public string DescricaoResumida =>
@@ -11,7 +16,7 @@ class Musica
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {Nome}");
-        System.Console.WriteLine($"Artista: {Artista}");
+        System.Console.WriteLine($"Artista: {Artista.Nome}");
         System.Console.WriteLine($"Duração: {Duracao}");
         if (Disponivel)
         {
--- Program.cs
+++ Program.cs
@@ -1,15 +1,25 @@
-﻿Album albumDoQueen = new Album();
-albumDoQueen.Nome = "A Night At The Opera";
+﻿Banda queen = new Banda("Queen");
 
-Musica musica1 = new Musica();
-musica1.Nome = "Love of My Life";
-musica1.Duracao = 213;
+Album albumDoQueen = new Album("A Night At The Opera");
 
-Musica musica2 = new Musica();
-musica2.Nome = "Bohemian Rhapsody";
-musica2.Duracao = 354;
+Musica musica1 = new Musica(queen, "Love of My Life")
+{
+    Duracao = 213,
+    Disponivel = true,
+};
+
+Musica musica2 = new Musica(queen, "Bohemian Rhapsody")
+{
+    Duracao = 354,
+    Disponivel = false,
+};
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+queen.AdicionarAlbum(albumDoQueen);
 
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
 albumDoQueen.ExibirMusicasDoAlbum();
+
+queen.ExibirDiscografia();
